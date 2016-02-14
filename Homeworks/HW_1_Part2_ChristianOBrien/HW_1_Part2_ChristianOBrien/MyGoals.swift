a217a0ddb27361f79d6ff744c5613fd76e9a4cd0
christianobrien <christian.obrien.345@gmail.com>
2016-02-13 22:23:00 -0500

    Homework 1 part 2

---
+++ Homeworks/HW_1_Part2_ChristianOBrien/HW_1_Part2_ChristianOBrien/MyGoals.swift
@@ -0,0 +1,14 @@
+//
+//  MyGoals.swift
+//  HW_1_Part2_ChristianOBrien
+//
+//  Created by Christian O'Brien on 2/10/16.
+//  Copyright © 2016 ChristianOBrien. All rights reserved.
+//
+
+import UIKit
+
+class MyGoals: UIViewController {
+
+    
+}
